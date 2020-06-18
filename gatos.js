34a08b38b9201c2eb4b513c9c136968cd22d7e4b
enivaldo bonelli <45775341+fisicaetcetera@users.noreply.github.com>
2020-06-18 15:02:59 -0300

    Add files via upload

---
+++ gatos.js
@@ -0,0 +1,16 @@
+var gato[];
+function preload(){
+  gato = loadJSON("./gatinho.json");
+}
+function setup() {
+  createCanvas(400, 400); 
+  console.log(gatinho);
+}
+function draw() {
+  background(0);
+  fill(255,110,20);
+  text(gato[0].nome, 10,50);
+  text(gato[1].nome, 10, 70);
+  text(gato[0].revolution, 10, 90);
+  text(gato[0].raiva, 10,110);
+}
